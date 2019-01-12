32d949be758bd0c5cd8d5e703e13ad7bf7525dfc
Charu Agarwal <charu13a@gmail.com>
2019-01-12 15:18:25 +0530

    Added search engine

--- user.php
+++ user.php
@@ -28,7 +28,20 @@
       <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
     </ul>
     <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-
+<div>
+ <script>
+  (function() {
+    var cx = '018427458728593593842:yxpdlc3tiji';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+<gcse:search></gcse:search>
+</div>
       <div class="row">
       <div class="col s12 m6">
         <div class="card large">
@@ -69,6 +82,7 @@
         </div>
       </div>
     </div>
+    
         <!--JavaScript at end of body for optimized loading-->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
