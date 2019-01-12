92283ba0a2c63899cce7a936dfbc67878c954de1
Charu Agarwal <charu13a@gmail.com>
2019-01-12 16:38:43 +0530

    Added timetable

--- user.php
+++ user.php
@@ -24,8 +24,8 @@
       <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
       <li><a href="#!">Second Link</a></li>
       <li><div class="divider"></div></li>
-      <li><a class="subheader">Subheader</a></li>
-      <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+      <li><a class="subheader">Important Resources</a></li>
+      <li><a class="waves-effect" href="files/2016-timetable.pdf">Timetable</a></li>
     </ul>
     <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
 <div>
